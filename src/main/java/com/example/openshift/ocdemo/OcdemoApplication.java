c9db699c00f5b9e872bb294a6b34d7b90b6e3f06
yhimg <yhimg@github.com>
2025-04-02 17:02:08 +0100

    Initial commit with Dockerfile

---
+++ src/main/java/com/example/openshift/ocdemo/OcdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.openshift.ocdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcdemoApplication.class, args);
+	}
+
+}
